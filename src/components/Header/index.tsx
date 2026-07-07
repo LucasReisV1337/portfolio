88db24418009680c869a03e92546512a85779554
Lucas Reis <86315092+LucasReisV1337@users.noreply.github.com>
2026-07-07 15:14:04 -0300

    Update WhatsApp link in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -109,7 +109,7 @@ export function Header() {
               <FiLinkedin />
             </a>
           </Link>
-          <Link href={'https://api.whatsapp.com/send?phone=5571988934202'}>
+          <Link href={'https://api.whatsapp.com/send?phone=5571986758027'}>
             <a
               target="_blank"
               aria-label="Link para entrar em contato via whatsapp"
